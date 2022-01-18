aca3f7e3ff7c5954e7106bbee3fcc3673561c6cb
k-irill-c <c.kirill@outlook>
2022-01-19 01:36:44 +0300

    My first5 commit in 4 sprint whis qes

--- yatube/users/urls.py
+++ yatube/users/urls.py
@@ -13,14 +13,14 @@ app_name = 'users'
 
 urlpatterns = [
     path(
-         'login/',
-         LoginView.as_view(template_name='users/login.html'),
-         name='login'
-     ),
+        'login/',
+        LoginView.as_view(template_name='users/login.html'),
+        name='login'
+    ),
     path(
-      'logout/',
-      LogoutView.as_view(template_name='users/logged_out.html'),
-      name='logout'
+        'logout/',
+        LogoutView.as_view(template_name='users/logged_out.html'),
+        name='logout'
     ),
     path('signup/', views.SignUp.as_view(), name='signup'),
     path(
