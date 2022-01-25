fc2a98f830708b10677156ef0417d1f355fceccf
k-irill-c <c.kirill@outlook>
2022-01-25 11:24:33 +0300

    My 3.1  commit

--- yatube/posts/forms.py
+++ yatube/posts/forms.py
@@ -10,14 +10,14 @@ class PostForm(forms.ModelForm):
         help_texts = {
             "text": "Текст нового поста",
             "group": "Группа, к которой будет относиться пост",
-            }
+        }
         group = forms.ModelChoiceField(
             queryset=Post.objects.all(),
             required=False, to_field_name="group"
-            )
+        )
         widgets = {
             'text': forms.Textarea(),
-            }
+        }
         labels = {
             "text": "Текст поста",
             "group": "Группа",
